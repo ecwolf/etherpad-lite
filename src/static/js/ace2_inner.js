07b5305b472e5bc0e5e244293e9c70c0bb10973a
Richard Hansen <rhansen@rhansen.org>
2020-11-22 17:55:25 -0500

    editor: Add argument to suppress SonarCloud error
    This also makes it easier for devs to understand the expected function
    signature.

--- src/static/js/ace2_inner.js
+++ src/static/js/ace2_inner.js
@@ -90,7 +90,7 @@ function Ace2Inner() {
 
   let outsideKeyDown = noop;
 
-  let outsideKeyPress = function () { return true; };
+  let outsideKeyPress = function (e) { return true; };
 
   let outsideNotifyDirty = noop;
 
